362161eb206dd325bda4094a539d0af01e3b02ac
Randal T Meyer <randal.t.meyer@gmail.com>
2024-01-30 15:29:15 -0800

    fix typings

--- build/parse/parseIds.d.ts
+++ build/parse/parseIds.d.ts
@@ -1,5 +1,5 @@
-import { Snowflake } from "@rsc-utils/snowflake-utils";
-import { DMessage } from "../types.js";
+import { type Snowflake } from "@rsc-utils/snowflake-utils";
+import { type DMessage } from "../types.js";
 type IdType = "channel";
 export declare function parseIds(message: DMessage, type: IdType): Snowflake[];
 export {};
