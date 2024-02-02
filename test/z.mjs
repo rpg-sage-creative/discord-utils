0ebff6be349312d8852ccfa4ca9cf9acef86c8ac
Randal T Meyer <randal.t.meyer@gmail.com>
2024-02-02 11:40:29 -0800

    update to test-utils

---
+++ test/z.mjs
@@ -0,0 +1,11 @@
+import { debug, info, warn, error } from "@rsc-utils/console-utils";
+import { assert, runTests, startAsserting, stopAsserting } from "@rsc-utils/test-utils";
+
+runTests(async function testOthers() {
+	error("Create mention tests!");
+	error("Create parseIds tests!");
+	error("Create url tests!");
+	error("Create DiscordKey tests!");
+	error("Create errorHandler tests!");
+	error("Create toHumanReadable tests!");
+}, true);
