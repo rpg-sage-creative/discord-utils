158c0b18b28c810c6ab889c11271df7b607df8f8
Randal T Meyer <randal.t.meyer@gmail.com>
2024-03-11 09:28:55 -0700

    sonar cleanup

--- build/url/toInviteUrl.js
+++ build/url/toInviteUrl.js
@@ -1,10 +1,12 @@
 import { error } from "@rsc-utils/console-utils";
 function isTextChannel(channel) {
     if (channel) {
-        if ("isText" in channel && typeof (channel.isText) === "function" && channel.isText())
+        if ("isText" in channel && typeof (channel.isText) === "function" && channel.isText()) {
             return true;
-        if ("isTextBased" in channel && typeof (channel.isTextBased) === "function" && channel.isTextBased())
+        }
+        if ("isTextBased" in channel && typeof (channel.isTextBased) === "function" && channel.isTextBased()) {
             return true;
+        }
     }
     return false;
 }
@@ -14,10 +16,12 @@ export function toInviteUrl(guild) {
     }
     try {
         const bestInvite = guild.invites.cache.find(invite => {
-            if ("stageInstance" in invite && invite.stageInstance)
+            if (("stageInstance" in invite) && invite.stageInstance) {
                 return false;
-            if (invite.targetUser || invite.temporary)
+            }
+            if (invite.targetUser || invite.temporary) {
                 return false;
+            }
             return isTextChannel(invite.channel);
         });
         return bestInvite?.url ?? null;
