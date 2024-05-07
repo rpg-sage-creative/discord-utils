c94cc891fd157fbc8a356d5971faa00f3190f626
Randal T Meyer <randal.t.meyer@gmail.com>
2024-05-07 14:16:47 -0700

    include recipients for multidm

--- build/typeChecks.js
+++ build/typeChecks.js
@@ -5,7 +5,7 @@ export function canFetchWebhooksFor(channel) {
     return channel ? "fetchWebhooks" in channel : false;
 }
 export function isDMBased(channel) {
-    return channel ? "recipient" in channel : false;
+    return channel ? ("recipient" in channel) || ("recipients" in channel) : false;
 }
 export function isGuildBased(channel) {
     const types = ["GUILD_TEXT", "GUILD_PRIVATE_THREAD", "GUILD_PUBLIC_THREAD", "GUILD_FORUM"];
