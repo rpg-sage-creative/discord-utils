7465c8b41ea5bddff1395e94c5f80b6e60ca8d7e
Randal <randal.t.meyer@gmail.com>
2024-07-18 14:37:56 -0700

    allow MessageOrPartial

--- src/resolve/resolveChannelReference.ts
+++ src/resolve/resolveChannelReference.ts
@@ -1,10 +1,11 @@
 import type { Optional, OrUndefined, Snowflake } from "@rsc-utils/core-utils";
-import type { Channel, Message } from "discord.js";
+import type { Channel } from "discord.js";
 import type { DiscordKey } from "../DiscordKey.js";
+import type { MessageOrPartial } from "../types/types.js";
 
 export type ChannelReference = { guildId?:Snowflake; channelId:Snowflake; }
 
-export type ChannelReferenceResolvable = Channel | Message | ChannelReference;
+export type ChannelReferenceResolvable = Channel | MessageOrPartial | ChannelReference;
 
 export type CanBeChannelReferenceResolvable = ChannelReferenceResolvable | DiscordKey; //NOSONAR
 
