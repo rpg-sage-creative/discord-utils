df5ff47c1e1d5e0c0015bfbeaea3bbdbb35ea96f
Randal <randal.t.meyer@gmail.com>
2024-07-30 16:13:02 -0700

    update options for addShortText and addParagraph

--- build/modal/ModalBuilder.d.ts
+++ build/modal/ModalBuilder.d.ts
@@ -1,5 +1,15 @@
+import type { Optional } from "@rsc-utils/core-utils";
 import { ModalBuilder as _ModalBuilder, TextInputBuilder } from "discord.js";
+type Options = {
+    maxLength?: number;
+    required?: boolean;
+};
 export declare class ModalBuilder extends _ModalBuilder {
-    addShortText(required?: boolean): TextInputBuilder;
-    addParagraph(required?: boolean): TextInputBuilder;
+    addShortText(): TextInputBuilder;
+    addShortText(required: Optional<boolean>): TextInputBuilder;
+    addShortText(options: Optional<Options>): TextInputBuilder;
+    addParagraph(): TextInputBuilder;
+    addParagraph(required: Optional<boolean>): TextInputBuilder;
+    addParagraph(options: Optional<Options>): TextInputBuilder;
 }
+export {};
--- build/modal/ModalBuilder.js
+++ build/modal/ModalBuilder.js
@@ -1,15 +1,27 @@
 import { ModalBuilder as _ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
 export class ModalBuilder extends _ModalBuilder {
-    addShortText(required) {
+    addShortText(arg) {
         const index = this.components.length;
         const row = new ActionRowBuilder();
         const input = new TextInputBuilder();
-        input.setStyle(TextInputStyle.Short).setCustomId(`field-${index}`).setRequired(required);
+        input.setStyle(TextInputStyle.Short);
+        input.setCustomId(`field-${index}`);
+        if (arg) {
+            if (typeof (arg) === "boolean") {
+                input.setRequired(arg === true);
+            }
+            else {
+                input.setRequired(arg?.required === true);
+                if (arg.maxLength) {
+                    input.setMaxLength(arg.maxLength);
+                }
+            }
+        }
         row.addComponents(input);
         this.addComponents(row);
         return input;
     }
-    addParagraph(required) {
-        return this.addShortText(required).setStyle(TextInputStyle.Paragraph);
+    addParagraph(arg) {
+        return this.addShortText(arg).setStyle(TextInputStyle.Paragraph);
     }
 }
