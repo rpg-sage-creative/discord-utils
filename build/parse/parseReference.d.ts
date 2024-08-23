49e0370142ef3f43773f633ab3e4cca91374c3ca
Randal <randal.t.meyer@gmail.com>
2024-08-23 16:59:59 -0700

    allow parseReference to accept Message and Channel objects

--- build/parse/parseReference.d.ts
+++ build/parse/parseReference.d.ts
@@ -1,6 +1,9 @@
 import type { Optional } from "@rsc-utils/core-utils";
-import type { MessageReference } from "discord.js";
+import type { Channel, MessageReference } from "discord.js";
+import type { MessageOrPartial } from "../types/types.js";
 type ReferenceType = "channel" | "message";
-/** Parses a discord message url into a MessageReference. */
-export declare function parseReference(url: Optional<string>, type: ReferenceType): MessageReference | undefined;
+/** Parses a discord message url into a MessageReference based on the type of url expected. */
+export declare function parseReference(value: Optional<string>, type: ReferenceType): MessageReference | undefined;
+/** Parses a discord Message or Channel into a MessageReference. */
+export declare function parseReference(value: Optional<Channel | MessageOrPartial>): MessageReference | undefined;
 export {};
--- build/parse/parseReference.js
+++ build/parse/parseReference.js
@@ -1,16 +1,38 @@
 import { unwrap } from "@rsc-utils/string-utils";
 import { createDiscordUrlRegex } from "./createDiscordUrlRegex.js";
-export function parseReference(url, type) {
-    if (url) {
-        const regex = createDiscordUrlRegex(type);
-        const match = regex.exec(unwrap(url, "<>"));
-        if (match?.groups) {
-            let { guildId, channelId, messageId } = match.groups;
-            if (guildId === "@me") {
-                guildId = undefined;
-            }
-            return { guildId, channelId, messageId };
+function parseString(url, type) {
+    const regex = createDiscordUrlRegex(type);
+    const match = regex.exec(unwrap(url, "<>"));
+    if (match?.groups) {
+        let { guildId, channelId, messageId } = match.groups;
+        if (guildId === "@me") {
+            guildId = undefined;
         }
+        return { guildId, channelId, messageId };
+    }
+    return undefined;
+}
+function parseMessage(message) {
+    return {
+        guildId: message.guildId ?? undefined,
+        channelId: message.channelId,
+        messageId: message.id
+    };
+}
+function parseChannel(channel) {
+    return {
+        guildId: "guildId" in channel ? channel.guildId ?? undefined : undefined,
+        channelId: channel.id,
+        messageId: undefined
+    };
+}
+export function parseReference(value, type) {
+    if (value) {
+        if (typeof (value) === "string")
+            return parseString(value, type ?? "message");
+        if ("channelId" in value)
+            return parseMessage(value);
+        return parseChannel(value);
     }
     return undefined;
 }
