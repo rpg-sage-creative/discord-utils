fe0bcf017bd9d8ee3af70ec9d72dabb7d9b182ef
Randal <randal.t.meyer@gmail.com>
2024-12-05 11:13:03 -0800

    update to use toLiteral

--- test/toString.mjs
+++ test/toString.mjs
@@ -1,12 +1,3 @@
-/** Converts the value into a human readable string intended for test output. */
-export function toString(value) {
-	if (value && (value instanceof RegExp)) {
-		return `/${value.source}/${value.flags}`;
-	}
-	switch (typeof(value)) {
-		case "bigint": return `${value}n`;
-		case "object": return JSON.stringify(value);
-		case "string": return JSON.stringify(value);
-		default: return String(value);
-	}
-}
+import { toLiteral } from "@rsc-utils/core-utils";
+/** @deprecated stop using this and start using toLiteral */
+export const toString = toLiteral;
