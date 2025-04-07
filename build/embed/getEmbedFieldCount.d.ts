1b5add8d83aedb6fae207057cf11fc1fe5eb5a8c
Randal <randal.t.meyer@gmail.com>
2025-04-07 13:30:33 -0700

    add comment

--- build/embed/getEmbedFieldCount.d.ts
+++ build/embed/getEmbedFieldCount.d.ts
@@ -1,3 +1,4 @@
 import type { Optional } from "@rsc-utils/core-utils";
 import type { EmbedResolvable } from "./EmbedResolvable.js";
+/** Counts the fields in the given EmbedResolvable, returning 0 if one wasn't given. */
 export declare function getEmbedFieldCount(resolvable: Optional<EmbedResolvable>): number;
