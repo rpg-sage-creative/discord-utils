cc790126bf195e8d1f36cf7aa46398efaef7f69c
Randal <randal.t.meyer@gmail.com>
2025-05-30 11:47:29 -0700

    streamline

--- build/url/urlOrUndefined.js
+++ build/url/urlOrUndefined.js
@@ -1,11 +1,9 @@
 import { unwrap } from "@rsc-utils/core-utils";
 import { isUrl } from "@rsc-utils/io-utils";
 export function urlOrUndefined(value) {
-    if (value) {
-        const bool = isUrl(value, { wrapChars: "<>", wrapOptional: true });
-        if (bool) {
-            return unwrap(value, "<>");
-        }
+    const bool = isUrl(value, { wrapChars: "<>", wrapOptional: true });
+    if (bool) {
+        return unwrap(value, "<>");
     }
     return undefined;
 }
--- src/url/urlOrUndefined.ts
+++ src/url/urlOrUndefined.ts
@@ -3,11 +3,9 @@ import { isUrl, type VALID_URL } from "@rsc-utils/io-utils";
 
 /** Checks that the valud is a valid url and returns the unwrapped url (removes <>). */
 export function urlOrUndefined(value: Optional<string>): VALID_URL | undefined {
-	if (value) {
-		const bool = isUrl(value, { wrapChars:"<>", wrapOptional:true });
-		if (bool) {
-			return unwrap(value, "<>") as VALID_URL;
-		}
+	const bool = isUrl(value, { wrapChars:"<>", wrapOptional:true });
+	if (bool) {
+		return unwrap(value, "<>") as VALID_URL;
 	}
 	return undefined;
 }
