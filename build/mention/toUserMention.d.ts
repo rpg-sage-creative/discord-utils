1b03aeb40b357b272339dd96f48722d0678fa856
Randal <randal.t.meyer@gmail.com>
2025-12-17 17:47:12 -0800

    update toUserMention signature

--- build/mention/toUserMention.d.ts
+++ build/mention/toUserMention.d.ts
@@ -1,4 +1,5 @@
 import type { Optional } from "@rsc-utils/core-utils";
 import { GuildMember, User } from "discord.js";
 import { type SnowflakeResolvable } from "../resolve/resolveSnowflake.js";
+export declare function toUserMention(resolvable: SnowflakeResolvable | GuildMember | User): string;
 export declare function toUserMention(resolvable: Optional<SnowflakeResolvable | GuildMember | User>): string | undefined;
